e51bffda3cc1d951e0ae3598e87335b5a5799dfa
edmarfjr <edmarjr@outlook.com>
2018-11-06 13:09:22 -0200

    Updates

---
+++ precache-manifest.bc55cc32a99836f539c8d42d7b5e3151.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "87dd12fc65c8c089eec01cf3880d053d",
+    "url": "/portifolio/static/media/spaceHeroin.87dd12fc.gif"
+  },
+  {
+    "revision": "0565eee0d77b2425eb0b",
+    "url": "/portifolio/static/css/main.364185b6.chunk.css"
+  },
+  {
+    "revision": "e561557787b027788fe57c0cdd90c1ff",
+    "url": "/portifolio/static/media/oc.e5615577.jpg"
+  },
+  {
+    "revision": "2ba597f94efd74115700",
+    "url": "/portifolio/static/js/1.2ba597f9.chunk.js"
+  },
+  {
+    "revision": "02a436588872a4a66e58",
+    "url": "/portifolio/static/js/runtime~main.02a43658.js"
+  },
+  {
+    "revision": "4d48a4a2650217c9766d431254bb3452",
+    "url": "/portifolio/static/media/edmar.4d48a4a2.jpg"
+  },
+  {
+    "revision": "a8e9876086e57423e426e8ade48152ed",
+    "url": "/portifolio/static/media/curriculum-vitae.a8e98760.pdf"
+  },
+  {
+    "revision": "0565eee0d77b2425eb0b",
+    "url": "/portifolio/static/js/main.0565eee0.chunk.js"
+  },
+  {
+    "revision": "e14c3f44648da61b8744b56051c8fa33",
+    "url": "/portifolio/static/media/mockuparknek.e14c3f44.png"
+  },
+  {
+    "revision": "169906cbc36167bf6eb829461663b805",
+    "url": "/portifolio/static/media/SpellSword.169906cb.gif"
+  },
+  {
+    "revision": "bde5902bbfb5cc509da08abfb57473c5",
+    "url": "/portifolio/static/media/volca.bde5902b.jpg"
+  },
+  {
+    "revision": "bcea9dd659dfafb3a6a2ecdc974a4cfc",
+    "url": "/portifolio/static/media/balder.bcea9dd6.jpg"
+  },
+  {
+    "revision": "2ba597f94efd74115700",
+    "url": "/portifolio/static/css/1.d9968422.chunk.css"
+  },
+  {
+    "revision": "d707af77da802f2ecb4db39c473f173d",
+    "url": "/portifolio/index.html"
+  }
+];
